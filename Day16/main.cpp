8f217e37998968a9180bcb0bcc408a769c88f2c0
Jose Gómez-Sellés <jldgso@gmail.com>
2021-12-16 23:03:37 +0100

    Part 2 Day16

--- Day16/main.cpp
+++ Day16/main.cpp
@@ -6,6 +6,9 @@
 #include <map>
 #include <sstream>
 #include <bitset>
+#include <numeric>
+
+size_t global_version_sum{0};
 
 std::string read_input()
 {
@@ -33,7 +36,7 @@ size_t extract_n(std::string &str, const size_t n)
 {
     std::string v = str.substr(0, n);
     str.erase(0, n);
-    return std::bitset<64>(v).to_ulong();
+    return std::bitset<128>(v).to_ulong();
 }
 
 size_t extract_one(std::string &str)
@@ -43,7 +46,7 @@ size_t extract_one(std::string &str)
     return v;
 }
 
-std::size_t extract_lit_dec(std::string &str)
+size_t extract_lit_dec(std::string &str)
 {
     bool b_cont = true;
     std::string lit_str;
@@ -57,84 +60,113 @@ std::size_t extract_lit_dec(std::string &str)
         }
         lit_str.append(processing.substr(1));
     }
-    return std::bitset<64>(lit_str).to_ulong();
+    return std::bitset<128>(lit_str).to_ulong();
 }
 
-size_t extract_operator(std::string &str);
+std::vector<size_t> extract_operator(std::string &str);
 
 size_t extract_subpacket(std::string &str)
 {
-    size_t version = extract_n(str, 3);
-    if (extract_n(str, 3) == 4)
+    global_version_sum += extract_n(str, 3);
+    if (size_t type = extract_n(str, 3); type == 4)
     {
-        extract_lit_dec(str);
+        return extract_lit_dec(str);
     }
     else
     {
-        version += extract_operator(str);
+        const auto v = extract_operator(str);
+        switch (type)
+        {
+        case 0:
+            return std::accumulate(v.cbegin(), v.cend(), 0l);
+            break;
+        case 1:
+            return std::accumulate(v.cbegin(), v.cend(), 1l, std::multiplies<size_t>());
+            break;
+        case 2:
+            return *std::min_element(v.begin(), v.end());
+            break;
+        case 3:
+            return *std::max_element(v.begin(), v.end());
+            break;
+        case 5:
+            return v.front() > v.back();
+            break;
+        case 6:
+            return v.front() < v.back();
+            break;
+        case 7:
+            return v.front() == v.back();
+            break;
+        default:
+            std::cout << "Type not found!\n";
+            exit(1);
+        }
     }
-    return version;
 }
 
-size_t extract_n_subpackets(std::string &str, const size_t n)
+std::vector<size_t> extract_n_subpackets(std::string &str, const size_t n)
 {
-    size_t versions{0};
+    std::vector<size_t> v_subpackets;
     for (size_t i = 0; i < n; i++)
     {
-        versions += extract_subpacket(str);
+        v_subpackets.push_back(extract_subpacket(str));
     }
-    return versions;
+    return v_subpackets;
 }
 
-size_t extract_l_subpackets(std::string &str, const size_t l)
+std::vector<size_t> extract_l_subpackets(std::string &str, const size_t l)
 {
-    size_t versions{0};
+    std::vector<size_t> v_subpackets;
+
     std::string subpackets = str.substr(0, l);
     str.erase(0, l);
     while (subpackets.size())
     {
-        versions += extract_subpacket(subpackets);
+        v_subpackets.push_back(extract_subpacket(subpackets));
     }
-    return versions;
+    return v_subpackets;
 }
 
-size_t extract_operator(std::string &str)
+std::vector<size_t> extract_operator(std::string &str)
 {
-    std::size_t versions{0};
+    std::vector<size_t> subpackets;
     switch (extract_one(str))
     {
     case 0:
-        versions += extract_l_subpackets(str, std::bitset<15>(extract_n(str, 15)).to_ulong());
+        subpackets = extract_l_subpackets(str, std::bitset<15>(extract_n(str, 15)).to_ulong());
         break;
     case 1:
-        versions += extract_n_subpackets(str, std::bitset<11>(extract_n(str, 11)).to_ulong());
+        subpackets = extract_n_subpackets(str, std::bitset<11>(extract_n(str, 11)).to_ulong());
         break;
     default:
         std::cout << "Error! " << std::endl;
         exit(1);
         break;
     }
-    return versions;
+    return subpackets;
 }
 
 void part_1()
 {
     const auto t_start = std::chrono::high_resolution_clock::now();
     auto input = read_input();
-    size_t versions{0};
+    size_t res{0};
     while (input.size())
     {
-        versions += extract_subpacket(input);
         if (size_t one_pos = input.find_first_of('1'); one_pos == input.npos)
         {
             break;
         }
+        res += extract_subpacket(input);
     }
 
     const auto t_stop = std::chrono::high_resolution_clock::now();
     const auto duration = std::chrono::duration_cast<std::chrono::microseconds>(t_stop - t_start);
 
-    std::cout << "Part 1: " << versions << '\n';
+    std::cout << "Part 1: " << global_version_sum << '\n';
+    std::cout << "Part 2: " << res << '\n';
+
     std::cout << "It took " << duration.count() << "us \n";
     return;
 }
